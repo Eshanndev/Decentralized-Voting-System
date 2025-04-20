d519100c636975fd67f416e7ccd4931d58ad4e5c
eshanndev <induwrabandara@gmail.com>
2025-04-20 13:36:13 +0530

    flow logic initialized

--- src/voting.sol
+++ src/voting.sol
@@ -9,6 +9,7 @@ contract Voting {
     error alreadyRegistered();
     error votingIsNotOpen();
     error registeringIsNotOpenYet();
+    error VotingIsNOtClosedYet();
     error votingIsNotEnded();
     error notAuthorized();
     error alreadyVoted();
@@ -33,6 +34,12 @@ contract Voting {
         ENDED
     }
 
+    enum AutomationTasks{
+        OPEN_REGISTRATION,
+        OPEN_VOTING,
+        OPEN_COUNTING
+    }
+
     /*///////////////////////////////////////////////
                 Mappings
     ////////////////////////////////////////////////*/
@@ -45,14 +52,21 @@ contract Voting {
                 State variables
     ////////////////////////////////////////////////*/
 
+
     address[] private qulifiedVoters;
     VotingStatus public currentVotingStatus = VotingStatus.OPEN;
     address immutable i_admin;
-    uint256[] public currentCanditates;
+    // uint256[] public currentCanditates;
     uint256 currentCanditate_1;
     uint256 currentCanditate_2;
     uint256 recentWinner;
     uint256 registerOpenedTime;
+    uint256 votingOpenedTime;
+    uint256 constant private REGISTERING_OPEN_TIME = 3600;
+    uint256 constant private VOTING_OPEN_TIME = 3600;
+    
+    uint256 latestWinner;
+
 
     constructor() {
         i_admin = msg.sender;
@@ -85,8 +99,8 @@ contract Voting {
         currentCanditate_2 =_canditate_2;
 
         
-        currentCanditates.push(_canditate_1);
-        currentCanditates.push(_canditate_2);
+        // currentCanditates.push(_canditate_1);
+        // currentCanditates.push(_canditate_2);
         voteCounts[_canditate_1] = 0;
         voteCounts[_canditate_2] = 0;
 
@@ -95,6 +109,7 @@ contract Voting {
 
         emit canditatesAdded(_canditate_1,_canditate_2);
     }
+
 
     function registerVoting() public {
         if (currentVotingStatus != VotingStatus.REGISTERING) {
@@ -127,14 +142,59 @@ contract Voting {
         voteCounts[_canditateId] += 1;
     }
 
-    function checkUpkeep(bytes memory) internal returns (bool upkeepNeeded, bytes memory) {
-     
-      
+    function votingResults()public{
+
+        if(currentVotingStatus != VotingStatus.CLOSED){
+            revert VotingIsNOtClosedYet();
+        }
+
+        if (voteCounts[currentCanditate_1] == voteCounts[currentCanditate_2]){
+            recentWinner = 0;
+        }else if(voteCounts[currentCanditate_1] < voteCounts[currentCanditate_2]){
+            recentWinner = currentCanditate_2;
+        }else {
+            recentWinner =  currentCanditate_1;
+        }
+        
+        resetVotingRound();
+        currentVotingStatus = VotingStatus.ENDED;
+
+    }
+
+    function checkUpkeep(bytes calldata) external view returns (bool upkeepNeeded, bytes memory data) {
+        if (block.timestamp - registerOpenedTime >  REGISTERING_OPEN_TIME){
+            upkeepNeeded = true;
+            data = abi.encode(AutomationTasks.OPEN_VOTING);
+        }
+        else if (block.timestamp - votingOpenedTime > VOTING_OPEN_TIME){
+            upkeepNeeded = true;
+            data = abi.encode(AutomationTasks.OPEN_COUNTING);
+        }
+
     }
 
 
-    function performUpkeep(bytes memory) public {
-        currentVotingStatus = VotingStatus.CLOSED;
+    function performUpkeep(bytes memory performData) public {
+        AutomationTasks task = abi.decode(performData, (AutomationTasks));
+
+        if (task == AutomationTasks.OPEN_VOTING){
+            currentVotingStatus = VotingStatus.OPEN;
+            votingOpenedTime = block.timestamp;
+        }
+        else if (task == AutomationTasks.OPEN_COUNTING){
+            currentVotingStatus = VotingStatus.CLOSED;
+            votingResults();
+        }
+    }
+
+    function resetVotingRound()public {
+        for (uint256 i = 0; qulifiedVoters.length< i;i++){
+            address voter = qulifiedVoters[i];
+            isVoted[voter] = false;
+            isRegistered[voter] = false;
+        }
+        qulifiedVoters = new address[](0);
+
     }
 
     
@@ -149,5 +209,9 @@ contract Voting {
 
     function getAdmin() public view returns (address) {
         return i_admin;
+    }
+
+    function getRecentWinner()public view returns(uint256){
+        return recentWinner;
     }
 }
