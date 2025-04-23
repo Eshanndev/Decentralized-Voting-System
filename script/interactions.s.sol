499ff29e05a3d335a80625a94de906bf9a1777ed
eshanndev <induwrabandara@gmail.com>
2025-04-23 12:41:42 +0530

    Vote and setCandidates Interactions tested

--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -9,38 +9,50 @@ abstract contract CodeConstants {
     uint256 public MOCK_CANDITATE_1 = 123;
     uint256 public MOCK_CANDITATE_2 = 456;
 
-    address mostRecentDeployment = getRecentDeployment();
+    
 
-    function getRecentDeployment() public view returns (address) {
-        return DevOpsTools.get_most_recent_deployment("Voting", block.chainid);
-    }
+    
 }
 
 contract SetCanditates is CodeConstants, Script {
     function run() public {
+        address mostRecentDeployment = getMostRecentDeployment();
         vm.startBroadcast();
-        setCanditates();
+        runSetCanditates(mostRecentDeployment);
         vm.stopBroadcast();
     }
 
-    function setCanditates() public {
+    function runSetCanditates(address mostRecentDeployment) public {
         
-        Voting(mostRecentDeployment).setCanditates(MOCK_CANDITATE_1, MOCK_CANDITATE_2);
+        Voting(mostRecentDeployment).setCanditates(MOCK_CANDITATE_1,MOCK_CANDITATE_2);
         
     }
+
+    function getMostRecentDeployment()public view returns(address){
+        address mostRecentDeployment = DevOpsTools.get_most_recent_deployment("Voting", block.chainid);
+        return mostRecentDeployment;
+    }
+
+    //and need to create a func for get recent deployment and should pass ot from when calling inside the run func
 }
 
 contract Vote is CodeConstants, Script {
     function run() public {
+        address mostRecentDeployment = getMostRecentDeployment();
         vm.startBroadcast();
-        vote(MOCK_CANDITATE_1);
+        runVote(mostRecentDeployment);
         vm.stopBroadcast();
     }
 
-    function vote(uint256 canditateId) public {
+    function runVote(address mostRecentDeployment) public {
         
-        Voting(mostRecentDeployment).vote(canditateId);
+        Voting(mostRecentDeployment).vote(MOCK_CANDITATE_1);
         
+    }
+
+    function getMostRecentDeployment()public view returns(address){
+        address mostRecentDeployment = DevOpsTools.get_most_recent_deployment("Voting", block.chainid);
+        return mostRecentDeployment;
     }
 
     // contract RegisterUpkeep is Script{
--- test/intergration/TestInteractions.t.sol
+++ test/intergration/TestInteractions.t.sol
@@ -1,11 +1,49 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {SetCanditates, Vote} from "../../script/interactions.s.sol";
 import {Voting} from "../../src/voting.sol";
 import {DevOpsTools} from "@Cyfrin/foundry-devops/src/DevOpsTools.sol";
 
 contract TestInteractions is Test {
+
+  uint256 REGISTERING_OPEN_TIME = 3600;
+  // address public USER1 = makeAddr("user1");
+
+  SetCanditates setCanditates;
+  Vote vote;
+  Voting voting;
+  
+    function setUp()public {
+      setCanditates = new SetCanditates();
+      vote = new Vote();
+      
+      vm.prank(address(setCanditates));
+      voting = new Voting();
+    }
+
+    function test_interactions()public {
+      
+      setCanditates.runSetCanditates(address(voting));
+      assert(Voting.VotingStatus.REGISTERING == voting.getCurrentVotingStatus());
+
+      vm.prank(address(vote));// might change this after making a interaction contract for registering
+      voting.registerVoting();
+
+      vm.warp(block.timestamp + REGISTERING_OPEN_TIME + 1);
+      vm.roll(block.number + 1);
+
+      (bool upkeepNeeded, bytes memory data) = voting.checkUpkeep("");
+        if (upkeepNeeded) {
+            voting.performUpkeep(data);
+        }
+
+      
+      vote.runVote(address(voting));
+    }
+
+
+
     
 }
